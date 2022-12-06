ebe63709854db3d36a02e3fa9e87c00e61c05df5
frankwwu <frankwwu@hotmail.com>
2022-12-06 09:12:06 -0500

    YouTube Downloader

---
+++ YouTube/YouTubeDownloader.py
@@ -0,0 +1,13 @@
+from telnetlib import SE
+from pytube import YouTube
+from sys import argv
+
+if len(argv) > 1:
+    link = argv[1]
+    yt = YouTube(link)
+
+    print("Title: ", yt.title)
+    yd = yt.streams.get_highest_resolution()
+    yd.download()
+else:
+    print("Please provide the address.")
